b8bc199ccb8bc8c9026b7840982d2737f97d7d69
Developer nehimi <nehimiagulufa@gmail.com>
2026-01-25 17:52:30 +0300

    connect to modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Footer from './Footer.jsx'; //Import the footer component
 function App() {
   return (
     <div>
       <h1>Day 1: Hello React!</h1>
       <p>I am building this from scratch.</p>
+      {/* 2. Use the component like an HTML tag */}
+      <Footer />
     </div>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return(
+
+        <footer style={{marginTop:'20px',color:'gray'}}>
+            <hr />
+            <p>&copy; 2026 My React Learning Journey</p>
+        </footer>
+    )
+}
+export default Footer;
