46ec386bd00eb77df5e0a8668abeb12eab5657c5
Developer nehimi <nehimiagulufa@gmail.com>
2026-01-26 09:09:20 +0300

    props

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,11 +1,9 @@
-function Greeting(){
+function Greeting({name, topic}){
     return(
-        <>
-            <h3>Welcome to React Course</h3> 
-            <p>Learn React with us!</p>
-           
-        
-        </>
+        <div style={{border: '1px solid blue',margin:'10px',padding:'10px'}}>
+            <h3>Hello, {name}!</h3>
+            <p>Welcome to day 4 of your {topic} journey</p>
+        </div>
     )
 }
 export default Greeting
